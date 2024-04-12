110a5ff6d1bcef52f9986d17b1d6018b0f686ca6
imooon <imonidharia@gmail.com>
2024-04-11 20:45:54 -0400

    added sellerRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const listingRoutes = require('./listingRoutes');
+const sellerRoutes = require('./sellerRoutes'); // Imported sellerRoutes.js
 
 router.use('/users', userRoutes);
 router.use('/listing', listingRoutes);
+router.use('/sellers', sellerRoutes); // Included sellerRoutes
 
 module.exports = router;
