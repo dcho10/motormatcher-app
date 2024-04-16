59aa09c90082365cc01a0e11e5382944e798c1fa
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-04-16 00:42:23 -0400

    Finally figured out how to transfer to listing

--- public/js/listing.js
+++ public/js/listing.js
@@ -16,7 +16,7 @@ const listingForm = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace("/");
+            document.location.replace("/listing");
         } else {
             alert(response.statusText);
         }
